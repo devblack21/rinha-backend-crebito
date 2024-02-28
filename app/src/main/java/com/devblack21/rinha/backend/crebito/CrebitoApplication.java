546bbf49fd026a8ce891012365a3b6ef919e0a55
marcos <nyrocha018@gmail.com>
2024-02-27 23:08:30 -0300

    chore: adding version 0.0.2-stable

--- app/src/main/java/com/devblack21/rinha/backend/crebito/CrebitoApplication.java
+++ app/src/main/java/com/devblack21/rinha/backend/crebito/CrebitoApplication.java
@@ -3,8 +3,10 @@ package com.devblack21.rinha.backend.crebito;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableJpaRepositories
 @SpringBootApplication
+@EnableTransactionManagement
 public class CrebitoApplication {
 
 	public static void main(final String[] args) {
